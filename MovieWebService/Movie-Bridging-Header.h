07af8b504a05afdce17ba59d5945f1d58add29d4
Prateek <pratvick@gmail.com>
2018-05-31 16:38:44 +0530

    Fix project structure

--- MovieWebService/Movie-Bridging-Header.h
+++
@@ -1,4 +0,0 @@
-#import "Actor.h"
-#import "Director.h"
-#import "Film.h"
-#import "TappableLabel.h"
--- MovieWebService/Supporting Files/Movie-Bridging-Header.h
+++ MovieWebService/Supporting Files/Movie-Bridging-Header.h
@@ -1,4 +1,9 @@
+#ifndef Movie_Bridging_Header_h
+#define Movie_Bridging_Header_h
+
 #import "Actor.h"
 #import "Director.h"
 #import "Film.h"
+#import "TappableLabel.h"
 
+#endif
