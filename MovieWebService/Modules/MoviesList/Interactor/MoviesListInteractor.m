880c5a990a8a93e9815667b36f8278e22b56b313
Prateek <pratvick@gmail.com>
2018-06-05 01:53:27 +0530

    Refactor Movies list interactor

--- MovieWebService/Modules/MoviesList/Interactor/MoviesListInteractor.m
+++ MovieWebService/Modules/MoviesList/Interactor/MoviesListInteractor.m
@@ -17,32 +17,47 @@
 - (void)fetchFilmsList {
   MovieWebService *movieWebService = [MovieWebService new];
   [movieWebService getFilmDataWithCallback:^(NSDictionary *filmData) {
-    NSMutableArray *castsList = [[NSMutableArray alloc] init];
-    NSArray *castsData = filmData[@"cast"];
-    for (NSDictionary *castData in castsData) {
-      Actor *actor = [[Actor alloc] initWithName:castData[@"name"]
-                                      screenName:castData[@"screenName"]
-                                       biography:castData[@"biography"]
-                                     dateOfBirth:[NSDate dateWithTimeIntervalSince1970:[castData[@"dateOfBirth"] doubleValue]]
-                                     isNominated:[castData[@"nominated"] boolValue]];
-      [castsList addObject:actor];
-    }
-    NSDictionary *directorData = filmData[@"director"];
-    Director *director = [[Director alloc] initWithName:directorData[@"name"]
-                                              biography:directorData[@"biography"]
-                                            dateOfBirth:[NSDate dateWithTimeIntervalSince1970:[directorData[@"dateOfBirth"] doubleValue]]
-                                            isNominated:[directorData[@"nominated"] boolValue]];
-    Film *film = [[Film alloc] initWithName:[filmData[@"name"] copy]
-                                 filmRating:[filmData[@"filmRating"] doubleValue]
-                                  languages:[filmData[@"languages"] copy]
-                                releaseDate:[NSDate dateWithTimeIntervalSince1970:[filmData[@"releaseDate"] doubleValue]]
-                                     rating:[filmData[@"rating"] doubleValue]
-                                       cast:((castsList.count > 0) ? castsList : nil)
-                                   director:director
-                                isNominated:[filmData[@"nominated"] boolValue]];
+    NSArray *castsList = [self castsListWithFilmData:filmData];
+    Director *director = [self createDirectorWithFilmData:filmData];
+    Film *film = [self createFilmWithFilmData:filmData director:director cast:castsList];
     filmData = nil;
     [self.output fetchedFilms:@[film]];
   }];
+}
+
+- (NSArray *)castsListWithFilmData:(NSDictionary *)filmData {
+  NSMutableArray *castsList = [[NSMutableArray alloc] init];
+  NSArray *castsData = filmData[@"cast"];
+  for (NSDictionary *castData in castsData) {
+    Actor *actor = [[Actor alloc] initWithName:castData[@"name"]
+                                    screenName:castData[@"screenName"]
+                                     biography:castData[@"biography"]
+                                   dateOfBirth:[NSDate dateWithTimeIntervalSince1970:[castData[@"dateOfBirth"] doubleValue]]
+                                   isNominated:[castData[@"nominated"] boolValue]];
+    [castsList addObject:actor];
+  }
+  return castsList;
+}
+
+- (Director *)createDirectorWithFilmData:(NSDictionary *)filmData {
+  NSDictionary *directorData = filmData[@"director"];
+  return [[Director alloc] initWithName:directorData[@"name"]
+                              biography:directorData[@"biography"]
+                            dateOfBirth:[NSDate dateWithTimeIntervalSince1970:[directorData[@"dateOfBirth"] doubleValue]]
+                            isNominated:[directorData[@"nominated"] boolValue]];
+}
+
+- (Film *)createFilmWithFilmData:(NSDictionary *)filmData
+                        director:(Director *)director
+                            cast:(NSArray *)castsList {
+  return [[Film alloc] initWithName:[filmData[@"name"] copy]
+                         filmRating:[filmData[@"filmRating"] doubleValue]
+                          languages:[filmData[@"languages"] copy]
+                        releaseDate:[NSDate dateWithTimeIntervalSince1970:[filmData[@"releaseDate"] doubleValue]]
+                             rating:[filmData[@"rating"] doubleValue]
+                               cast:((castsList.count > 0) ? castsList : nil)
+                           director:director
+                        isNominated:[filmData[@"nominated"] boolValue]];
 }
 
 @end
